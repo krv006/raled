e5ab31ad50c8f2dac82d5a75bb7d562c89aa1ccf
samariddin <samariddin0304@gmail.com>
2024-06-04 21:08:31 +0500

    changes

--- core/settings.py
+++ core/settings.py
@@ -69,8 +69,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
